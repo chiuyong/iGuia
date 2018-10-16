721993210b111169d54b7515d393101a32a2de60
chiuyong <yongcchiu@gmail.com>
2018-10-16 14:00:48 -0300

    pagina inicial finalizado

--- iguiaapp/site/js/isotope/scripts.js
+++ iguiaapp/site/js/isotope/scripts.js
@@ -1,25 +1,15 @@
-$(function () {
-    /**************************************************************/
-    // Initialize Isotope
-    var $notes = $(".notes").isotope({
-        itemSelector: ".note"
-        , layoutMode: 'packery'
-    });
-    /**************************************************************/
-    /****************************************************************/
-    //on filter button click
-    $(".menu .my_btn").click(function (e) {
-        var $this = $(this);
-        //    prevent default behaviour
-        e.preventDefault();
-        //get the data attribute from the button
-        var $filter = $this.attr("data-filter")
-        $notes.isotope({
-            filter: $filter
-        });
-        //add/remove class
-        $(".menu .my_btn").removeClass("btn_active");
-        $this.addClass("btn_active")
-    });
-    /****************************************************************/
+
+//Switch Content button click
+$('#btn_Guide').on('click',function(){
+		$('#howToUseTourist').hide();
+		$('#howToUseGuide').show();
+		$("#btn_Guide").attr('class', 'btn_active');
+		$( "#btn_Tourist" ).removeClass( "btn_active" );
 });
+$('#btn_Tourist').on('click',function(){
+		$('#howToUseGuide').hide();
+		$('#howToUseTourist').show();
+		$("#btn_Tourist").attr('class', 'btn_active');
+		$( "#btn_Guide" ).removeClass( "btn_active" );		
+});
+
